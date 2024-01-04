9b6e00330ee1767b21c12db2a55b16582a9911ae
Alex Hepburn <ah13558@it044201.users.bris.ac.uk>
2024-01-04 16:59:38 +0000

    added starting a new episode and changed feedback to be buttons ready for experiments.

--- flask_app.py
+++ flask_app.py
@@ -1,7 +1,7 @@
 import uuid
-import logging
 import base64
 from io import BytesIO
+import gc
 
 from flask import Flask
 from flask import render_template, request, session, Response
@@ -16,7 +16,6 @@ import matplotlib
 matplotlib.use('agg')
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 
-import pandas as pd
 import numpy as np
 
 from envPacMan import env
@@ -25,7 +24,7 @@ from agent import agent
 store = RedisStore(redis.StrictRedis())
 app = Flask(__name__)
 KVSessionExtension(store, app)
-app.secret_key = "test"
+app.secret_key = "test2"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////home/ah13558/Documents/taku_pacman/RL-Alex/result_database.sqlite"
 db = SQLAlchemy(app)
 
@@ -58,18 +57,17 @@ def base64EncodeFigure(fig):
     buf = BytesIO()
     fig.tight_layout()
     fig.savefig(buf, format='png', bbox_inches='tight', facecolor='#2F2E2E')
-    del fig
     data = base64.b64encode(buf.getbuffer()).decode('ascii')
+    buf.flush()
+    buf.seek(0)
+    plt.close()
+    # gc.collect()
     return data
 
 
-@app.route('/')
-def index():
-    # initialse user
-    session['uid'] = uuid.uuid4()
-    # Initialise Environment
+def initialise_environment():
     environment = env()
-    environment.reset(random=False)
+    environment.reset(random=True)
     disp = environment.display()
 
     algID   = 'tabQL_ps_Cest'
@@ -84,10 +82,8 @@ def index():
     fb = np.ones(len(C)) * np.NaN # Human feedback
     current_environment = environment.display()
 
-    fig, ax = environment.plot()
-    fig2 = plt.Figure(figsize=(5, 5))
+    fig, _ = environment.plot()
     data = base64EncodeFigure(fig)
-    data2 = base64EncodeFigure(fig2)
     # Save to session
     session['environment'] = environment
     session['current_environment'] = data
@@ -108,10 +104,60 @@ def index():
     session['disp'] = disp
     session['Ce_list'] = [0.5]
 
-    fig3, ax = plot_confidence(session['Ce_list'])
+
+
+def new_environment_plots():
+    fig, _ = session['environment'].plot()
+    fig2 = plt.Figure(figsize=(5, 5))
+    data = base64EncodeFigure(fig)
+    data2 = base64EncodeFigure(fig2)
+
+    fig3, _ = plot_confidence(session['Ce_list'])
     data3 = base64EncodeFigure(fig3)
+    return data, data2, data3
+
+
+@app.route('/')
+def index():
+    # initialse user
+    session['uid'] = uuid.uuid4()
+    initialise_environment()
+    data, data2, data3 = new_environment_plots()
     session['graph_data'] = data3
-    return render_template("index.html", img1=data2, img2=data, graph=data3)
+    return render_template("index.html",
+                           img1=data2,
+                           img2=data,
+                           graph=data3,
+                           session_status=session['status'][-1])
+
+
+@app.route('/newepisode', methods=['POST'])
+def new_episode():
+    session['environment'].reset()
+    session['agent'].prev_obs = []
+
+    # Reset environment
+    fig, _ = session['environment'].plot()
+    data = base64EncodeFigure(fig)
+    session['current_environment'] = data
+    session['current_environment_idx'] = 0
+    session['environment_display_list'] = [data]
+    session['environment_integer_list'] = [session['environment'].st2ob()]
+    session['action_taken_list'] = [-1]
+    session['current_action'] = -1
+    session['obs'] = [session['environment'].st2ob()]
+    session['rws'] = [0]
+    session['totRW'] = 0
+    session['status'] = [False]
+    disp = ''.join([x+'\n' for x in session['environment'].display()])
+    session['disp'] = disp
+
+    data, data2, data3 = new_environment_plots()
+    return render_template("index.html",
+                           img1=data2,
+                           img2=data,
+                           graph=data3,
+                           session_status=session['status'][-1])
 
 
 @app.route('/next', methods=['POST'])
@@ -136,7 +182,7 @@ def nextFrame():
             session['obs'].append(ob)
             session['rws'].append(rw)
             session['status'].append(status)
-            fig, ax = session['environment'].plot()
+            fig, _ = session['environment'].plot()
             data = base64EncodeFigure(fig)
             session['current_environment'] = data
             session['environment_display_list'].append(data)
@@ -152,7 +198,8 @@ def nextFrame():
     return render_template("index.html",
                            img1=data2,
                            img2=session['current_environment'],
-                           graph=session['graph_data'])
+                           graph=session['graph_data'],
+                           session_status=session['status'][-1])
 
 
 @app.route('/previous', methods=['POST'])
@@ -170,7 +217,8 @@ def previousFrame():
     return render_template("index.html",
                            img1=data2,
                            img2=session['current_environment'],
-                           graph=session['graph_data'])
+                           graph=session['graph_data'],
+                           session_status=session['status'][-1])
 
 
 @app.route('/submit', methods=['POST'])
@@ -181,51 +229,43 @@ def submit():
         data2 = base64EncodeFigure(fig)
     else:
         # for plotting
-        feedback = [request.form['Feedback%d'%i] if request.form.get('Checkbox%d'%i) else
-                    'None' for i in range(1,5)]
+        feedback_string = request.form['feedback_button']
         action_list = session['action_list']
-        feedback = dict(map(lambda i,j : (i, j), action_list, feedback))
         idx = session['current_environment_idx']
-        if any(key!='None' for key in feedback.values()):
-            # If feedback is provided, then we train the agent for one
-            # iteration.
-            a = max(feedback, key=lambda k: float(feedback[k] if feedback[k]!='None' else 0.0))
-            # Get integer corresponding to the action
-            action = action_list.index(a)
-            taken_action = session['action_taken_list'][session['current_environment_idx']]
-            fb = [1.0] if action == taken_action else [0.0]
-            # Check what action the agent would take - check if it's the same as human feedback - 
-            # 0 or 1 for feedback to the agent.
-            _ = session['agent'].act(
-                    action,
-                    session['obs'][idx],
-                    session['rws'][idx],
-                    session['status'][idx],
-                    fb,
-                    0.5,
-                    skip_confidence=False)
-            print(session['Ce_list'])
-            print(session['agent'].Ce)
-            session['Ce_list'].append(session['agent'].Ce[0])
-            fig, ax = plot_confidence(session['Ce_list'])
-            graph_data = base64EncodeFigure(fig)
-            session['graph_data'] = graph_data
+        # Get integer corresponding to the action
+        action = action_list.index(feedback_string)
+        taken_action = session['action_taken_list'][session['current_environment_idx']]
+        fb = [1.0] if action == taken_action else [0.0]
+        # Check what action the agent would take - check if it's the same as human feedback - 
+        # 0 or 1 for feedback to the agent.
+        _ = session['agent'].act(
+                action,
+                session['obs'][idx],
+                session['rws'][idx],
+                session['status'][idx],
+                fb,
+                0.5,
+                skip_confidence=False)
+        session['Ce_list'].append(session['agent'].Ce[0])
+        fig, ax = plot_confidence(session['Ce_list'])
+        graph_data = base64EncodeFigure(fig)
+        session['graph_data'] = graph_data
 
-            sessionID = session['uid']
-            idx = session['current_environment_idx']
-            env = session['environment_integer_list'][idx-1]
-            action = session['action_list'][session['current_action']]
-            entry = annotations(
-                sessionID=str(sessionID), env=int(env), action=action,
-                feedback=str(feedback))
-            db.session.add(entry)
-            db.session.commit()
-            data2 = session['environment_display_list'][session['current_environment_idx']-1]
-
+        sessionID = session['uid']
+        idx = session['current_environment_idx']
+        env = session['environment_integer_list'][idx-1]
+        action = session['action_list'][session['current_action']]
+        entry = annotations(
+            sessionID=str(sessionID), env=int(env), action=action,
+            feedback=feedback_string)
+        db.session.add(entry)
+        db.session.commit()
+        data2 = session['environment_display_list'][session['current_environment_idx']-1]
     return render_template("index.html",
                            img1=data2,
                            img2=session['current_environment'],
-                           graph=graph_data)
+                           graph=graph_data,
+                           session_status=session['status'][-1])
 
 
 if __name__ == "__main__":
